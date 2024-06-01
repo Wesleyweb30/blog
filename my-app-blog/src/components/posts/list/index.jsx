fcae4898998d6a5064235fe09b81cc5466fc0af2
Wesley <147221712+Wesleyweb30@users.noreply.github.com>
2024-06-01 19:38:55 -0300

    Create Component PostsList

---
+++ my-app-blog/src/components/posts/list/index.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { Col, Pagination, Row } from "react-bootstrap";
+import PostCard from "../card";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios"
+import PostForm from "../form";
+
+
+export default function PostsList() {
+
+    const POSTS_PAGE = 12;
+    const [currentPage, setCurrentPage] = useState(1);
+
+
+    const { isIdle, isLoading, isError, data, error } = useQuery({
+        queryKey: ['postsData', currentPage],
+        queryFn: () =>
+            axios.get('https://jsonplaceholder.typicode.com/posts')
+                .then(response => {
+                    const start = (currentPage - 1) * POSTS_PAGE;
+                    const end = start + POSTS_PAGE;
+                    return response.data.slice(start, end)
+                })
+    });
+
+    console.log(data)
+    if (isIdle || isLoading) return 'Loading...';
+    if (isError) return 'An error has occurred: ' + error.message;
+    const totalPages = Math.ceil(110 / POSTS_PAGE);  
+
+    return (
+        <>
+            <Row className="mb-3">
+                <Col className="d-flex justify-content-end">
+                    <PostForm/>
+                </Col>
+            </Row>
+            <Row xs={1} md={2} lg={3} xl={3} xxl={3}>
+                {data.map(post => (
+                    <Col key={post.id}>
+                        <PostCard title={post.title} user={post.UserId} body={post.body} />
+                    </Col>
+                ))}
+            </Row>
+            <Row>
+                <Pagination className="d-flex justify-content-center">
+                    <Pagination.Prev onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))} />
+                    {Array.from({ length: totalPages }, (_, idx) => (
+                        <Pagination.Item
+                            key={idx + 1}
+                            active={idx + 1 === currentPage}
+                            onClick={() => setCurrentPage(idx + 1)}
+                        >
+                            {idx + 1}
+                        </Pagination.Item>
+                    ))}
+                    <Pagination.Next onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))} />
+                </Pagination>
+            </Row>
+
+        </>
+    );
+}
--- my-app-blog/src/page/home/index.jsx
+++ my-app-blog/src/page/home/index.jsx
@@ -1,38 +1,10 @@
-import React from 'react';
-import Nav from "../../components/nav";
-import { Container, Col, Row } from "react-bootstrap";
-import PostCard from "../../components/posts/card";
-import { useQuery } from "@tanstack/react-query";
-import axios from 'axios'; // Importando Axios
+import PostsList from "../../components/posts/list";
+
 
 export default function HomePage() {
-  const { isIdle, isLoading, isError, data, error } = useQuery({
-    queryKey: ['postsData'],
-    queryFn: () =>
-      axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.data.slice(0, 12))
-  });
-
-  console.log(data)
-  if (isIdle || isLoading) return 'Loading...';
-  if (isError) return 'An error has occurred: ' + error.message;
-
   return (
     <>
-      <Nav />
-      <main>
-        <section>
-          <Container >
-            <Row xs={1} md={2} lg={3} xl={3} xxl={3}> {/* Definindo quantas colunas em cada breakpoint */}
-              {data.map(post => (
-                <Col key={post.id}>
-                  <PostCard title={post.title} user={post.UserId} body={post.body} />
-                </Col>
-              ))}
-            </Row>
-          </Container>
-        </section>
-      </main>
+      <PostsList/>
     </>
   );
 }
